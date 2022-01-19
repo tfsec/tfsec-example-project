9ea6587898d06390125953f6790a67bc95583420
Owen Rumney <owen.rumney@aquasec.com>
2022-01-19 11:40:44 +0000

    use the commenter and add an issue

--- modules/acme_bucket/main.tf
+++ modules/acme_bucket/main.tf
@@ -4,7 +4,7 @@ data "aws_s3_bucket" "logging_bucket" {
 
 resource "aws_s3_bucket" "acme_bucket" {
   bucket = var.bucket_name
-
+  acl    = "public-read"
   logging {
     target_bucket = data.aws_s3_bucket.logging_bucket.id
     target_prefix = format("%s/logs/", var.bucket_name)
