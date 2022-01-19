fa358f79c87fe25b62b2f689efcf7d63cecc84af
Owen Rumney <owen.rumney@aquasec.com>
2022-01-19 14:14:47 +0000

    readd

--- modules/acme_bucket/main.tf
+++ modules/acme_bucket/main.tf
@@ -10,4 +10,11 @@ resource "aws_s3_bucket" "acme_bucket" {
     target_prefix = format("%s/logs/", var.bucket_name)
   }
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
