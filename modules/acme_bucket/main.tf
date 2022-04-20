a6d68a1ae6794fce4014490d7fed3a5e4d3c91d3
Owen Rumney <owen.rumney@aquasec.com>
2022-04-20 15:31:57 +0100

    feat: clean up

--- modules/acme_bucket/main.tf
+++ modules/acme_bucket/main.tf
@@ -1,14 +1,6 @@
-data "aws_s3_bucket" "logging_bucket" {
-  bucket = var.s3_logging_bucket
-}
 
 resource "aws_s3_bucket" "acme_bucket" {
   bucket = var.bucket_name
-
-  logging {
-    target_bucket = data.aws_s3_bucket.logging_bucket.id
-    target_prefix = format("%s/logs/", var.bucket_name)
-  }
 
   server_side_encryption_configuration {
     rule {
